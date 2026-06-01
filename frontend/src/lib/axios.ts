54cb9d073e63d2033d2e9386dd27368efa1e58f8
Erro <errisbinxhija781@gmail.com>
2026-06-02 00:46:52 +0200

    Add dotenv dependency and configure API base URL dynamically

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: `http://localhost:${PORT}/api`,
   withCredentials: true,
 });
